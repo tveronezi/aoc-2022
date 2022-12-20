82616a00c9c73fc2bcdda299c41f333f1c3ad378
Thiago Veronezi <tveronezi@gmail.com>
2022-12-20 15:02:20 -0500

    day 4

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,17 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod day5;
 mod error;
 
 /// Input files
 pub mod input;
 
-use std::str::FromStr;
-
 use day1::group_max;
 use day2::{CheatRpsMatch, RpsMatch};
 use day3::{priority, Rucksack};
+use day4::AssignmentPair;
 use day5::{ActionsLines, Warehouse};
 use error::Ooops;
 
@@ -94,64 +94,6 @@ pub fn the_sum_of_the_priorities_for_shared_item_types_in_three_elfs_group(value
         .sum()
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct AssignmentRange {
-    start: usize,
-    end: usize,
-}
-
-impl AssignmentRange {
-    fn fully_contains(&self, other: &Self) -> bool {
-        self.start <= other.start && self.end >= other.end
-    }
-
-    fn overlaps(&self, other: &Self) -> bool {
-        (other.start >= self.start && other.start <= self.end)
-            || (other.end >= self.start && other.end <= self.end)
-    }
-}
-
-impl FromStr for AssignmentRange {
-    type Err = Ooops;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let values = s.split('-');
-        let values = values.collect::<Vec<&str>>();
-        let mut values = values.iter();
-        let map_err = |e| Ooops(format!("{}", e));
-        match (values.next(), values.next()) {
-            (Some(start), Some(end)) => Ok(AssignmentRange {
-                start: start.parse().map_err(map_err)?,
-                end: end.parse().map_err(map_err)?,
-            }),
-            (_, _) => Err(Ooops(format!("bad range {}", s))),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct AssignmentPair {
-    a: AssignmentRange,
-    b: AssignmentRange,
-}
-
-impl FromStr for AssignmentPair {
-    type Err = Ooops;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let values = s.split(',');
-        let values = values.collect::<Vec<&str>>();
-        let mut values = values.iter();
-        match (values.next(), values.next()) {
-            (Some(a), Some(b)) => Ok(Self {
-                a: a.parse()?,
-                b: b.parse()?,
-            }),
-            (_, _) => Err(Ooops(format!("bad pair {}", s))),
-        }
-    }
-}
-
 /// Part A -> <https://adventofcode.com/2022/day/4>
 pub fn how_many_pairs_does_one_fully_contain_the_other(values: &str) -> usize {
     values
@@ -186,56 +128,4 @@ pub fn crates_on_top_of_each_stack(values: &str) -> Result<String, Ooops> {
         warehouse.shuffle(&action);
     }
     Ok(warehouse.top_crates())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn parse_assignment_range() {
-        assert_eq!(Ok(AssignmentRange { start: 2, end: 4 }), "2-4".parse());
-        assert_eq!(Ok(AssignmentRange { start: 6, end: 8 }), "6-8".parse());
-    }
-
-    #[test]
-    fn assignment_range_contains() {
-        assert!(AssignmentRange { start: 2, end: 4 }
-            .fully_contains(&AssignmentRange { start: 2, end: 4 }));
-        assert!(AssignmentRange { start: 2, end: 4 }
-            .fully_contains(&AssignmentRange { start: 3, end: 4 }));
-        assert!(AssignmentRange { start: 2, end: 4 }
-            .fully_contains(&AssignmentRange { start: 4, end: 4 }));
-        assert!(AssignmentRange { start: 2, end: 4 }
-            .fully_contains(&AssignmentRange { start: 2, end: 3 }));
-        assert!(AssignmentRange { start: 2, end: 4 }
-            .fully_contains(&AssignmentRange { start: 2, end: 2 }));
-        assert!(!AssignmentRange { start: 2, end: 4 }
-            .fully_contains(&AssignmentRange { start: 1, end: 4 }));
-    }
-
-    #[test]
-    fn assignment_range_overlaps() {
-        assert!(
-            AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 2, end: 4 })
-        );
-        assert!(
-            AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 3, end: 4 })
-        );
-        assert!(
-            AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 0, end: 4 })
-        );
-        assert!(
-            AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 0, end: 3 })
-        );
-        assert!(
-            AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 4, end: 6 })
-        );
-        assert!(
-            !AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 0, end: 1 })
-        );
-        assert!(
-            !AssignmentRange { start: 2, end: 4 }.overlaps(&AssignmentRange { start: 6, end: 10 })
-        );
-    }
 }
