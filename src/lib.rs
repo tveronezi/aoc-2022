e52572d534b459db4305f6d13830106a30637a23
Thiago Veronezi <tveronezi@gmail.com>
2022-12-20 14:53:56 -0500

    day 3

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,18 @@
 
 mod day1;
 mod day2;
+mod day3;
 mod day5;
 mod error;
 
 /// Input files
 pub mod input;
 
-use std::{collections::HashSet, str::FromStr};
+use std::str::FromStr;
 
 use day1::group_max;
 use day2::{CheatRpsMatch, RpsMatch};
+use day3::{priority, Rucksack};
 use day5::{ActionsLines, Warehouse};
 use error::Ooops;
 
@@ -29,63 +31,6 @@ pub fn total_of_calories_for_the_top_three_elfs(values: &str) -> usize {
     values.reverse();
     let values = values.iter();
     values.take(3).sum()
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct Rucksack {
-    compartment_a: String,
-    compartment_b: String,
-    shared: HashSet<char>,
-}
-
-impl Rucksack {
-    fn intersection(&self, others: Vec<&Rucksack>) -> HashSet<char> {
-        let this = format!("{}{}", self.compartment_a, self.compartment_b)
-            .chars()
-            .collect::<HashSet<char>>();
-        let mut result = this;
-
-        for other in others {
-            let other = format!("{}{}", other.compartment_a, other.compartment_b)
-                .chars()
-                .collect::<HashSet<char>>();
-            result = result.intersection(&other).map(|v| v.to_owned()).collect();
-        }
-        result
-    }
-}
-
-impl FromStr for Rucksack {
-    type Err = Ooops;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (compartment_a, compartment_b) = s.split_at(s.len() / 2);
-        if compartment_a.len() != compartment_b.len() {
-            return Err(Ooops(format!(
-                "compartments don't have the same number of elements. {}:{}",
-                compartment_a, compartment_b
-            )));
-        }
-        let mut shared = HashSet::new();
-        for c in compartment_a.chars() {
-            if compartment_b.contains(c) {
-                shared.insert(c);
-            }
-        }
-        Ok(Self {
-            compartment_a: compartment_a.into(),
-            compartment_b: compartment_b.into(),
-            shared,
-        })
-    }
-}
-
-fn priority(c: &char) -> Result<usize, Ooops> {
-    let index = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".find(*c);
-    if index.is_none() {
-        return Err(Ooops(format!("{} is not valid", c)));
-    }
-    Ok(index.unwrap())
 }
 
 /// Part A -> <https://adventofcode.com/2022/day/2>
@@ -246,124 +191,6 @@ pub fn crates_on_top_of_each_stack(values: &str) -> Result<String, Ooops> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn split_rucksack() {
-        assert_eq!(
-            Rucksack {
-                compartment_a: "vJrwpWtwJgWr".into(),
-                compartment_b: "hcsFMMfFFhFp".into(),
-                shared: HashSet::from(['p'])
-            },
-            "vJrwpWtwJgWrhcsFMMfFFhFp".parse().unwrap()
-        );
-        assert_eq!(
-            Rucksack {
-                compartment_a: "jqHRNqRjqzjGDLGL".into(),
-                compartment_b: "rsFMfFZSrLrFZsSL".into(),
-                shared: HashSet::from(['L']),
-            },
-            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".parse().unwrap()
-        );
-        assert_eq!(
-            Rucksack {
-                compartment_a: "PmmdzqPrV".into(),
-                compartment_b: "vPwwTWBwg".into(),
-                shared: HashSet::from(['P'])
-            },
-            "PmmdzqPrVvPwwTWBwg".parse().unwrap()
-        );
-        assert_eq!(
-            HashSet::from(['v']),
-            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
-                .parse::<Rucksack>()
-                .unwrap()
-                .shared
-        );
-        assert_eq!(
-            HashSet::from(['t']),
-            "ttgJtRGJQctTZtZT".parse::<Rucksack>().unwrap().shared
-        );
-        assert_eq!(
-            HashSet::from(['s']),
-            "CrZsJsPPZsGzwwsLwLmpwMDw"
-                .parse::<Rucksack>()
-                .unwrap()
-                .shared
-        );
-    }
-
-    #[test]
-    fn calculate_priority() {
-        assert_eq!(Ok(1), priority(&'a'));
-        assert_eq!(Ok(26), priority(&'z'));
-        assert_eq!(Ok(27), priority(&'A'));
-        assert_eq!(Ok(52), priority(&'Z'));
-        assert_eq!(Ok(16), priority(&'p'));
-        assert_eq!(Ok(38), priority(&'L'));
-        assert_eq!(Ok(42), priority(&'P'));
-        assert_eq!(Ok(22), priority(&'v'));
-        assert_eq!(Ok(20), priority(&'t'));
-        assert_eq!(Ok(19), priority(&'s'));
-    }
-
-    #[test]
-    fn calculate_shared_intersection() {
-        let one = "vJrwpWtwJgWrhcsFMMfFFhFp".parse::<Rucksack>().unwrap();
-        let two = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
-            .parse::<Rucksack>()
-            .unwrap();
-        let three = "PmmdzqPrVvPwwTWBwg".parse::<Rucksack>().unwrap();
-        assert_eq!(HashSet::from(['r']), one.intersection(vec![&two, &three]))
-    }
-
-    #[test]
-    fn calculate_shared_intersection_2() {
-        assert_eq!(
-            HashSet::from(['Z']),
-            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
-                .parse::<Rucksack>()
-                .unwrap()
-                .intersection(vec![
-                    &"ttgJtRGJQctTZtZT".parse().unwrap(),
-                    &"CrZsJsPPZsGzwwsLwLmpwMDw".parse().unwrap()
-                ])
-        )
-    }
-
-    #[test]
-    fn calculate_shared_intersection_3() {
-        assert_eq!(
-            52,
-            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
-                .parse::<Rucksack>()
-                .unwrap()
-                .intersection(vec![
-                    &"ttgJtRGJQctTZtZT".parse().unwrap(),
-                    &"CrZsJsPPZsGzwwsLwLmpwMDw".parse().unwrap()
-                ])
-                .iter()
-                .filter_map(|v| priority(v).ok())
-                .sum::<usize>()
-        )
-    }
-
-    #[test]
-    fn calculate_shared_intersection_4() {
-        assert_eq!(
-            18,
-            "vJrwpWtwJgWrhcsFMMfFFhFp"
-                .parse::<Rucksack>()
-                .unwrap()
-                .intersection(vec![
-                    &"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".parse().unwrap(),
-                    &"PmmdzqPrVvPwwTWBwg".parse().unwrap()
-                ])
-                .iter()
-                .filter_map(|v| priority(v).ok())
-                .sum::<usize>()
-        )
-    }
 
     #[test]
     fn parse_assignment_range() {
